979f25034b89232769c52c7555fa6e5ce46f8d77
chaintip <chaintip@protonmail.com>
2018-02-18 23:13:25 +0000

    Auto update

--- updater.py
+++ updater.py
@@ -59,9 +59,8 @@ for issue in bounties_repo.get_issues(state='open'):
         repo_string += " / "
         repo_string += "[%s](%s)" % (split[1], ctu(i['repo_url']))
         amount_usd = float(i['amount']) * price
-        readme += """$%7.2f | [[#%s](%s)] %s | %s | %s
-""" % (amount_usd, i['number'], ctu(i['url']), i['title'], repo_string, pulls_string)
-
+        readme += """$%s | [[#%s](%s)] %s | %s | %s
+""" % (round(amount_usd, 2), i['number'], ctu(i['url']), i['title'], repo_string, pulls_string)
 
 readme += """
 # Collected Bounties
@@ -80,13 +79,11 @@ for issue in bounties_repo.get_issues(state='closed'):
             repo_string += " / "
             repo_string += "[%s](%s)" % (split[1], ctu(i['repo_url']))
             amount_usd = float(i['amount']) * price
-            readme += """$%7.2f | [[#%s](%s)] %s | %s | %s
-""" % (amount_usd, i['number'], ctu(i['url']), i['title'], repo_string, pull_string)
+            readme += """$%s | [[#%s](%s)] %s | %s | %s
+""" % (round(amount_usd, 2), i['number'], ctu(i['url']), i['title'], repo_string, pull_string)
 
 with open("README.md", "w") as readme_file:
     print(readme, file = readme_file)
-
-print(readme)
 
 log.info(subprocess.check_output(['git','add', '.']))
 try:
